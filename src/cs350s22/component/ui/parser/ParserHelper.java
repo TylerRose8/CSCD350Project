bab80c124bb28edf5298c7abd84166cb936d6367
Ryan Golden <65426263+MeguminXIII@users.noreply.github.com>
2022-06-08 13:45:33 -0700

    Update ParserHelper.java

--- src/cs350s22/component/ui/parser/ParserHelper.java
+++ src/cs350s22/component/ui/parser/ParserHelper.java
@@ -4,12 +4,4 @@ public class ParserHelper extends A_ParserHelper
 {
    public ParserHelper()
    {  }
-
-   //Doing this so I don't get an error message don't think this is correct, 
-   //he said he is uploading another .jar so maybe exit will be ok then?
-   public void exit() {
-	   System.exit(0);
-	   return;
-   }  
-   
 }
