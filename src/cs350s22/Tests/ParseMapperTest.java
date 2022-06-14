51e302c393cf0344a222410254cad6d9af0e5800
Tyler Rose <trose8@ewu.edu>
2022-06-14 14:17:14 -0700

    Mapper test inherits from base

--- src/cs350s22/Tests/ParseMapperTest.java
+++ src/cs350s22/Tests/ParseMapperTest.java
@@ -11,60 +11,9 @@ import java.io.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ParseMapperTest {
-    File fileOut;
-    File fileErr;
-    FileWriter writerOut;
-    FileWriter writerErr;
-    PrintStream defaultOut;
-    PrintStream defaultErr;
-
+class ParseMapperTest extends TestBase {
     ParseMapperTest() throws IOException {
-        String testName = "Mapper";
-        fileOut = new File(testName+"TestLog.txt");
-        fileErr = new File(testName+"TestErrors.txt");
-        writerOut = new FileWriter(fileOut,true);
-        writerErr = new FileWriter(fileErr, true);
-    }
-
-    @BeforeEach
-    void setUp() {
-        PrintStream out = new PrintStream(new OutputStream()
-        {
-            @Override
-            public void write(int b)
-            {
-                try {
-                    writerOut.write(b);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        PrintStream err = new PrintStream(new OutputStream()
-        {
-            @Override
-            public void write(int b)
-            {
-                try {
-                    writerErr.write(b);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        defaultOut = System.out;
-        defaultErr = System.err;
-        System.setOut(out);
-        System.setErr(err);
-        System.out.println("Next Test");
-        System.out.println("==========================================================");
-    }
-
-    @AfterEach
-    void tearDown() {
-        System.setOut(defaultOut);
-        System.setErr(defaultErr);
+        super("Mapper");
     }
 
     @Test
