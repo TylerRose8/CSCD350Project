5500903ec3cb09d40b62a7f00401a229194263d3
Tyler Rose <trose8@ewu.edu>
2022-06-14 13:27:06 -0700

    Fix folder structure

--- src/cs350s22.component.ui.parser/ParserHelper.java
+++
@@ -1,8 +0,0 @@
-package cs350s22.component.ui.parser;
-
-public class ParserHelper extends A_ParserHelper {
-    public ParserHelper() {
-        super();
-        System.out.println("Welcome to your ParserHelper");
-    }
-}
--- src/cs350s22/component/ui/parser/ParserHelper.java
+++ src/cs350s22/component/ui/parser/ParserHelper.java
@@ -1,11 +1,8 @@
 package cs350s22.component.ui.parser;
 
-public class ParserHelper extends A_ParserHelper
-{
-   public ParserHelper()
-   {
-   }
-
-
-
+public class ParserHelper extends A_ParserHelper {
+    public ParserHelper() {
+        super();
+        System.out.println("Welcome to your ParserHelper");
+    }
 }
