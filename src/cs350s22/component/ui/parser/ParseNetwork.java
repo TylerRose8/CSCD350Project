025fbd4f198ec69895e0d1cd5075a23103b6dae4
Tyler Rose <trose8@ewu.edu>
2022-06-16 13:08:41 -0700

    Network Command Refactor

--- src/cs350s22/component/ui/parser/ParseNetwork.java
+++ src/cs350s22/component/ui/parser/ParseNetwork.java
@@ -1,64 +1,31 @@
 package cs350s22.component.ui.parser;
 
-import java.util.ArrayList;
-
-import cs350s22.component.*;
+import cs350s22.component.A_Component;
 import cs350s22.component.actuator.A_Actuator;
 import cs350s22.component.controller.A_Controller;
 import cs350s22.component.sensor.A_Sensor;
 import cs350s22.support.Identifier;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParseNetwork {
 
-	public static Object parseNetworkCommand(A_ParserHelper parserHelper, String input) {
-        String[] command = input.split(" ");
-		String type;
-		String val;
-		ArrayList<A_Component> list = new ArrayList<A_Component>();
-		A_Controller controller;
-		A_Actuator actuator;
-		A_Sensor sensor;
-		Identifier id;
+    public static void parseNetworkCommand(A_ParserHelper parserHelper, String input) {
+        ArrayList<A_Component> list = new ArrayList<>();
 
-        	type = command[4];
-        	switch(type) {
-        	case "COMPONENT":
-        		val = command[5];
-        		id = Identifier.make(val);
-        		controller = parserHelper.getSymbolTableController().get(id);
-        		actuator = parserHelper.getSymbolTableActuator().get(id);
-        		sensor = parserHelper.getSymbolTableSensor().get(id);
+        List<Identifier> components = Parser.getSetOfIds(input, "COMPONENT");
 
-        		list.add(controller);
-        		list.add(actuator);
-        		list.add(sensor);
-
-        		parserHelper.getControllerMaster().addComponents(list);
-        		parserHelper.getNetwork().writeOutput();
-        		break;
-
-        	case "COMPONENTS":
-        		for(int i = 5; i < command.length; i++) {
-        			val = command[i];
-            		id = Identifier.make(val);
-            		controller = parserHelper.getSymbolTableController().get(id);
-            		actuator = parserHelper.getSymbolTableActuator().get(id);
-            		sensor = parserHelper.getSymbolTableSensor().get(id);
-
-            		list.add(controller);
-            		list.add(actuator);
-            		list.add(sensor);
-
-            		parserHelper.getControllerMaster().addComponents(list);
-            		parserHelper.getNetwork().writeOutput();
-        		}
-        		break;
-
-        	}
-
-
-        return null;
-
-	}
-
+        for (Identifier component : components) {
+			if (parserHelper.getSymbolTableController().contains(component)) {
+				list.add(parserHelper.getSymbolTableController().get(component));
+			} else if (parserHelper.getSymbolTableActuator().contains(component)) {
+				list.add(parserHelper.getSymbolTableActuator().get(component));
+			} else if (parserHelper.getSymbolTableSensor().contains(component)) {
+				list.add(parserHelper.getSymbolTableSensor().get(component));
+			}
+		}
+		parserHelper.getControllerMaster().addComponents(list);
+		parserHelper.getNetwork().writeOutput();
+    }
 }
