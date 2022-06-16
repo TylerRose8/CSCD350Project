25a47f865566b06be13eca34393b728a55fe91de
Tyler Rose <trose8@ewu.edu>
2022-06-16 01:32:17 -0700

    Bounds checking and actuator commands

--- src/cs350s22/component/ui/parser/Parser.java
+++ src/cs350s22/component/ui/parser/Parser.java
@@ -57,9 +57,15 @@ public class Parser implements ParserConstants {
             return getSetOfIds(command, startKeyWord);
         }
 
+        //handle s vs no s
         if(!startKeyWord.toUpperCase().endsWith("S")){
+            String word = startKeyWord;
             startKeyWord = startKeyWord.toUpperCase() + "S";
+            if(!command.toUpperCase().contains(startKeyWord)) {
+                command = command.replaceFirst(word, startKeyWord);
+            }
         }
+
         String[] items = command.split(startKeyWord);
         items = items[1].split(endKeyWord)[0].trim().split(" ");
         for (String item : items) {
